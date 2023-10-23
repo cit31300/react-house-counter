9bcc11217f3f49ad19f46d7bb21ea6753662a026
Rob Elliott <elliott@iu.edu>
2023-10-23 15:25:27 -0400

    End of demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+function HouseCounter({ housename }) {
+  const [counter, setCounter] = useState(0);
+
+  const increase = (num) => {
+    setCounter(counter => counter + num);
+  };
+
+  const decrease = (num) => {
+    setCounter(counter => counter - num);
+  };
+
+  return (
+    <div className="col">
+      <div className="card text-center">
+        <h5 className="card-header">{ housename } Counter</h5>
+        <div className="card-body">
+          <h5 className="card-title">{ counter } points</h5>
+          <div>
+            <button 
+              type="button" 
+              className="btn btn-success"
+              onClick={() => {increase(1)}}
+            >+</button>
+            <button 
+              type="button" 
+              className="btn btn-danger"
+              onClick={() => {decrease(1)}}
+            >-</button>
+            <button 
+              type="button" 
+              className="btn btn-success"
+              onClick={() => {increase(10)}}
+            >+10</button>
+            <button 
+              type="button" 
+              className="btn btn-danger"
+              onClick={() => {decrease(10)}}
+            >-10</button>
+            <button 
+              type="button" 
+              className="btn btn-success"
+              onClick={() => {increase(20)}}
+            >+20</button>
+            <button 
+              type="button" 
+              className="btn btn-danger"
+              onClick={() => {decrease(20)}}
+            >-20</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row row-cols-2 row-cols-md-2 g-4">
+      <HouseCounter housename="Griffyndor" />
+      <HouseCounter housename="Slytherin" />
+      <HouseCounter housename="Hufflepuff" />
+      <HouseCounter housename="Ravenclaw" />
     </div>
   );
 }
